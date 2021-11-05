8e0a1d0a01281175c963e1f8ed494c0de445780c
Oumayma210 <abdellioumayma210@gmail.com>
2021-11-05 01:59:04 +0100

    checkk

--- client/src/pages/PublicPages/Home.js
+++ client/src/pages/PublicPages/Home.js
@@ -1,15 +1,6 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
+// import { Carousel } from "react-bootstrap";
 const Home = () => {
-    return (
-         <div  div className="pic-ctn">
-  <img src="https://picsum.photos/200/300?t=1" alt className="pic" />
-  <img src="https://picsum.photos/200/300?t=2" alt className="pic" />
-  <img src="https://picsum.photos/200/300?t=3" alt className="pic" />
-  <img src="https://picsum.photos/200/300?t=4" alt className="pic" />
-  <img src="https://picsum.photos/200/300?t=5" alt className="pic" />
-</div>
-    );
+    return <div>Home</div>;
 };
-
 export default Home;
